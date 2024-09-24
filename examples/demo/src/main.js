a78012eebb1035400f18112c108ed243a3566cba
zclzone <zclzone@outlook.com>
2024-09-24 15:39:39 +0800

    refactor: adjust the default options

--- examples/demo/src/main.js
+++ examples/demo/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
-import { routes } from './pages/generoutes'
+import { routes } from './router/routes'
 
 const router = createRouter({
   history: createWebHashHistory(),
