2d71104a1d90be7fb69045003e1f155396e5db38
zclzone <zclzone@outlook.com>
2026-01-04 16:14:11 +0800

    feat: update RouteMeta type definition

--- src/index.ts
+++ src/index.ts
@@ -150,7 +150,7 @@ export interface RouteMeta {
   /** Page code */
   code?: string
   /** Page layout */
-  layout?: string
+  layout?: string | boolean
   /** Whether authentication is required */
   requireAuth?: boolean
   /** Whether to keep alive */
